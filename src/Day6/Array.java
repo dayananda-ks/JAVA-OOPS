3b02874926466d54fb9cd60fea5fdae6690f354f
Dayananda  K S <dayanandaks045@gmail.com>
2026-05-03 17:04:18 +0530

    Update Array.java

--- src/Day6/Array.java
+++ src/Day6/Array.java
@@ -1,20 +1,32 @@
 package Day6;
 
+import com.sun.security.auth.NTSidUserPrincipal;
+
 import java.util.Arrays; // It is the Standard java libarary used for printing of All Elements in array. Without Using Standard library we print one by one by using index or key value.
 
+class arrayElm {
+    public void withoutArray(int a, int b, int c) {
+        System.out.println("a " + a);
+        System.out.println("b " + b);
+        System.out.println("c " + c);
+    }
+    public void withArray(int array []) {
+        System.out.print("Array : " + Arrays.toString(array));
+    }
+}
 
 public class Array {
-    
     public static void main(String[] args) {
+        // Without Array
         int a = 10;
         int b = 20;
         int c = 30;
-        System.out.println("a" + a);
-        System.out.println("b" + b);
-        System.out.println("c" + c);
+        // Array
         int arry [] = {10,20,30};
-        System.out.print(Arrays.toString(arry));
-
+        // Object creation for each type
+        arrayElm arr = new arrayElm();
+        arr.withoutArray(a, b, c);
+        arr.withArray(arry);
     }
 }
 
