d1e03cc04e3ea8fa37df1a69842e58877f7e51c2
Dayananda KS <dayanandaks045@gmail.com>
2026-05-03 17:08:46 +0530

    With and Without Array...

--- src/Day6/Array.java
+++ src/Day6/Array.java
@@ -5,6 +5,10 @@ import com.sun.security.auth.NTSidUserPrincipal;
 import java.util.Arrays; // It is the Standard java libarary used for printing of All Elements in array. Without Using Standard library we print one by one by using index or key value.
 
 class arrayElm {
+<<<<<<< HEAD
+=======
+
+>>>>>>> 44546f3 (With and Without Array...)
     public void withoutArray(int a, int b, int c) {
         System.out.println("a " + a);
         System.out.println("b " + b);
@@ -16,17 +20,32 @@ class arrayElm {
 }
 
 public class Array {
+
     public static void main(String[] args) {
+<<<<<<< HEAD
         // Without Array
+=======
+        //without Array
+>>>>>>> 44546f3 (With and Without Array...)
         int a = 10;
         int b = 20;
         int c = 30;
         // Array
         int arry [] = {10,20,30};
+<<<<<<< HEAD
         // Object creation for each type
         arrayElm arr = new arrayElm();
         arr.withoutArray(a, b, c);
         arr.withArray(arry);
+=======
+        //object creation for each type
+        arrayElm arr = new arrayElm();
+        arr.withoutArray(a, b, c);
+        arr.withArray(arry);
+
+
+
+>>>>>>> 44546f3 (With and Without Array...)
     }
 }
 
