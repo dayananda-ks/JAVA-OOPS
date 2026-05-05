9f95b9948f0e284ba33bbce2ab5b50668c0a3879
Dayananda KS <dayanandaks045@gmail.com>
2026-05-06 00:00:58 +0530

    ml array

--- src/Day8/MDArray.java
+++ src/Day8/MDArray.java
@@ -1,4 +1,10 @@
 package Day8;
 
+import java.util.Arrays;
+
 public class MDArray {
+    public static void main(String[] args) {
+        int array [][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println( Arrays.deepToString( array ) );
+    }
 }
