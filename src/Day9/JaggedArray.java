ab618a4499b322ae49da1694dfdec37d7461c1d4
Dayananda KS <dayanandaks045@gmail.com>
2026-05-06 19:07:18 +0530

    Declare and Initlized Jagged Array

--- src/Day9/JaggedArray.java
+++ src/Day9/JaggedArray.java
@@ -1,22 +1,15 @@
 package Day9;
-
+import java.util.Arrays;
 public class JaggedArray {
     public static void main(String[] args)
     {
-//		int nums[][]=new int [3][]; //jagged
-//		nums[0]=new int [3];
-//		nums[1]=new int [4];
-//		nums[2]=new int [2];
-
-        int nums[][]=new int [3][4];
-//		int nums[][]=new int [3][4][5]; // three dimensional
-
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums[i].length;j++)
-            {
-                nums[i][j]=(int)(Math.random()*10);
+        int array[][] = {{10,30},{5,8,7},{1,2,3,4}};
+        int len =  array.length;
+        for (int i =0; i<len; i++) {
+            for(int j = i;j<array[i].length;j++){
+                System.out.print(array[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
