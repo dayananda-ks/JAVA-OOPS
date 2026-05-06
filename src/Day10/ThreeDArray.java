3dd2a8aef5b62aa9363119b5ca11892913c7ab97
Dayananda KS <dayanandaks045@gmail.com>
2026-05-06 21:31:39 +0530

    3D Array Code

--- src/Day10/ThreeDArray.java
+++ src/Day10/ThreeDArray.java
@@ -1,12 +1,28 @@
 package Day10;
 
+import java.util.Arrays;
+
 public class ThreeDArray {
     public static void main(String[] args) {
-        int arr[][] = {
-                {1,2,3},
-                {4,5,6},
-                {7,8,9}
+        int arr[][][] = {
+                {
+                        {1,2,3},
+                        {4,5,6},
+                        {7,8,9}
+                },
+                {
+                        {10,20,30},
+                        {20,30,35},
+                        {100,200,500}
+                }
         };
-        System.out.println(arr[1][1]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.print(arr[i][j][k] + " ");
+                }System.out.println();
+            }
+        }
+
     }
 }
