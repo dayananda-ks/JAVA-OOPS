2068f88f037ce22b51a0be0197aebe7b3b460474
Dayananda KS <dayanandaks045@gmail.com>
2026-05-09 22:45:50 +0530

    Mutable and Immutable

---
+++ src/Day13/MutandImmut.java
@@ -0,0 +1,16 @@
+package Day13;
+
+public class MutandImmut {
+    public static void main(String[] args)
+    {
+        String name="navin";
+        name=name+"reddy";
+        System.out.println("hello"+name);
+
+        String s1="Navin";
+        String s2="Navin";
+
+        System.out.println(s1==s2);
+
+    }
+}
---
+++ src/DrawBacksOfArray.md
@@ -0,0 +1,10 @@
+
+
+
+### Drawbacks :-
+
+
+- The memory allocation is contiguous.
+- The size of an array is fixed. Array size cannot be expand.
+- Searching takes time.
+- Array can store values of only same type. It can store homogeneous type value only.
